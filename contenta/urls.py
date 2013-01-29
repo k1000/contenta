7a95bf77b30324e9694b19131ec8ff5115270781
k1000 <selwak@gmail.com>
2013-01-29 20:27:47 +0000

    renamed contento > contenta

--- contenta/urls.py
+++ contenta/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns
 
-urlpatterns = patterns('contento.views',
+urlpatterns = patterns('contenta.views',
 	(r'^json/(?P<url>.*)$', 'render_json'),
 	(r'^save(?P<url>.*)$', 'save_content'),
     (r'^(?P<url>.*)$', 'render_page'),
